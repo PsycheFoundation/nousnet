73c19d5aeebbfaaee107fea1b22a6fd7dea6aad5
rob-maron <132852777+rob-maron@users.noreply.github.com>
2026-02-26 15:35:27 -0500

    build and push tt-tp-test image

--- scripts/push-client-image.sh
+++ scripts/push-client-image.sh
@@ -3,8 +3,15 @@
 # into Docker's local storage, avoiding double disk usage.
 set -eu
 
+BRANCH="${GITHUB_REF_NAME:-main}"
+if [ "$BRANCH" = "main" ]; then
+    TAG="latest"
+else
+    TAG="$BRANCH"
+fi
+
 IMAGE_PATH=$(cat image-path.txt)
 "$IMAGE_PATH" | skopeo copy \
     --dest-creds "${REGISTRY_USERNAME}:${REGISTRY_PASSWORD}" \
     docker-archive:/dev/stdin \
-    "docker://${DOCKER_REPOSITORY}:latest"
+    "docker://${DOCKER_REPOSITORY}:${TAG}"
